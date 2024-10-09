a2571f44620a0f7fe8108be56bea494b5c368264
Akshay P <akshaybala2021@gmail.com>
2024-10-10 01:45:48 +0530

    10/10/2024

---
+++ android/app/src/main/kotlin/com/example/loginwork/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loginwork
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Loginpage.dart
@@ -0,0 +1,282 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:loginwork/Welcome.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _usernamecntr = TextEditingController();
+  final TextEditingController _passwordcntr = TextEditingController();
+  final AuthService _authService = AuthService();
+
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(color: Colors.black),
+          Positioned(
+            top: 55,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/img/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              height: MediaQuery.of(context).size.height - 50,
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/img/time.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                              child: Image.asset(
+                                'assets/img/Mobilesignal.png',
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                              child: Image.asset(
+                                'assets/img/Wifi.png',
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                              child: Image.asset(
+                                'assets/img/statusbattery.png',
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                              child: Image.asset(
+                                'assets/img/piano.png',
+                                width: 70,
+                                height: 70,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Image.asset(
+                                'assets/img/writing.png',
+                                width: 200,
+                                height: 200,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      SizedBox(height: 20),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        elevation: 4,
+                        child: TextFormField(
+                          controller: _usernamecntr,
+                          decoration: InputDecoration(
+                            hintText: "Username",
+                            hintStyle: TextStyle(color: Colors.white),
+                            filled: true,
+                            fillColor: Colors.grey[800],
+                            labelStyle: TextStyle(color: Colors.grey),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(
+                                color: Colors.grey[600]!,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(
+                                color: Colors.grey[400]!,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      // Password Field
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        elevation: 4,
+                        child: TextFormField(
+                          controller: _passwordcntr,
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.white),
+                            filled: true,
+                            fillColor: Colors.grey[800],
+                            labelStyle: TextStyle(color: Colors.grey),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(
+                                color: Colors.grey[600]!,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(
+                                color: Colors.grey[400]!,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          onTap: () async {
+                            bool success = await _authService.login(
+                                _usernamecntr.text.trim(),
+                                _passwordcntr.text.trim());
+
+                            if (success) {
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => WelcomePage(),
+                                ),
+                                (Route<dynamic> route) => false,
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content:
+                                      Text('Login failed. Please try again.'),
+                                ),
+                              );
+                            }
+                          },
+                          child: Container(
+                            height: 60,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.white,
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Create one",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Text(
+                          'By continuing, you are agreeing to our Terms and Conditions.',
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AuthService {
+  final String baseUrl = "https://musicapp.jissanto.com/api";
+
+  Future<bool> login(String username,String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/login'),
+        body: {'username': username,'password': password},
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['token'] != null) {
+          return true;
+        }
+      }
+      return false;
+    } catch (e) {
+      print("Error message: $e");
+      return false;
+    }
+  }
+}
